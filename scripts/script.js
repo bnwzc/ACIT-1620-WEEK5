0c3c7e4971b93ef610b5f298c1cb65047319cb8f
Zhu Kuangda <kzhu28@my.bcit.ca>
2023-03-19 02:38:56 -0700

    Define a constant array with 3 items

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+let course1 = {
+    code: "ACIT 1620",
+    name: "Web Fundamental Technologies"
+}
+let course2 = {
+    code: "ACIT 1515",
+    name: "Scripting for IT"
+}
+let course3 = {
+    code: "ACIT 1310",
+    name: "Math for IT"
+}
+
+
+const courseList = [course1, course2, course3]
