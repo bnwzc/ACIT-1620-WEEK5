616d55937af2f33affb1f1eb5e11530bcb0aee4e
Zhu Kuangda <kzhu28@my.bcit.ca>
2023-03-19 02:48:33 -0700

    get a 4 digits number from user

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,21 @@
 let course1 = {
     code: "ACIT 1620",
     name: "Web Fundamental Technologies"
-}
+};
 let course2 = {
     code: "ACIT 1515",
     name: "Scripting for IT"
-}
+};
 let course3 = {
     code: "ACIT 1310",
     name: "Math for IT"
-}
+};
 
 
-const courseList = [course1, course2, course3]
+const courseList = [course1, course2, course3];
+
+
+let user;
+do{
+    user = prompt("Type a 4 digits-code")
+}while((isNaN(user)) || (user.length != 4))
