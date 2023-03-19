2d22591165fba4dccff1559d3a38c24c258ba5df
Zhu Kuangda <kzhu28@my.bcit.ca>
2023-03-19 03:15:21 -0700

    Check if the course is in the list

--- scripts/script.js
+++ scripts/script.js
@@ -12,10 +12,40 @@ let course3 = {
 };
 
 
-const courseList = [course1, course2, course3];
+const courseList = [ 
+    {
+        code: "ACIT 1620",
+        name: "Web Fundamental Technologies"
+    },
+    {
+        code: "ACIT 1515",
+        name: "Scripting for IT"
+    },
+    {
+        code: "ACIT 1310",
+        name: "Math for IT"
+    }
+];
 
 
 let user;
 do{
     user = prompt("Type a 4 digits-code")
-}while((isNaN(user)) || (user.length != 4))
+}while((isNaN(user)) || (user.length != 4));
+
+
+judge = true;
+for(let course of courseList){
+    if (course["code"].includes(user) == true){
+        console.log(`Yes I am taking the course: ${course['code']} - ${course['name']}`)
+        judge = false
+    }
+};
+if(judge == true){
+    courseList.push({code:"ACIT " + user,name:null})
+    console.log(`The new course ACIT ${user} has been added` )
+}
+
+for(let course of courseList){
+    console.log(course)
+}
